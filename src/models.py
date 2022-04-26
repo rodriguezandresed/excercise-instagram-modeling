5142e166d71709cb151a8a8abb8bfee6857d311f
rodriguezandresed <rodriguezandresed@gmail.com>
2022-04-26 19:11:03 +0000

    [add] diagrama

--- src/models.py
+++ src/models.py
@@ -9,23 +9,26 @@ from eralchemy import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'User'
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     userId = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False, unique = True)
     password = Column(String(250), nullable=False)
     loginStatus = Column(Boolean, nullable=False)
     registrationDate = Column(Date, nullable=False)
+   
 
-class Favorites(Base):
-    __tablename__ = 'favorites'
+class Favorite(Base):
+    __tablename__ = 'favorite'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
+    user_id=Column(Integer, ForeignKey('user.id'))
     favoriteName = Column(String(250))
     favoriteNature = Column(String(250))
-
+    favoriteStatus = Column(Boolean)
+    user=relationship(User, back_populates="user")
 
 class Nature(Base):
     __tablename__ = 'nature'
@@ -35,12 +38,70 @@ class Nature(Base):
     natureName = Column(String(250))
 
 class Person(Base):
-    __tablename__ = 'Person'
+    __tablename__ = 'person'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     uid = Column(Integer, primary_key=True)
     personName = Column(String(250))
-    person = relationship(Nature)
+    personNature= Column(String(250), ForeignKey("nature.natureName"))
+    person = relationship(Nature,  back_populates="nature")
+    
+
+
+class PersonDetail(Base):
+    __tablename__ = 'person_detail'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    uid = Column(Integer, primary_key=True)
+    personName = Column(String(250))
+    detailNature = Column(String(250), ForeignKey("person.personNature") )
+    hairColor = Column(String(250))
+    favorited = Column(Boolean, ForeignKey("favorite.favoriteStatus") )
+    personDetail = relationship(Person,  back_populates="person")
+    favoriteStatus = relationship(Favorite, back_populates="favorite")
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    uid = Column(Integer, primary_key=True)
+    planetName = Column(String(250))
+    planetNature= Column(String(250), ForeignKey("nature.natureName"))
+    planet = relationship(Nature,  back_populates="nature")
+
+class PlanetDetail(Base):
+    __tablename__ = 'planet_detail'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    uid = Column(Integer, primary_key=True)
+    planetName = Column(String(250))
+    detailNature = Column(String(250), ForeignKey("planet.planetNature") )
+    population = Column(Integer)
+    favorited = Column(Boolean, ForeignKey("favorite.favoriteStatus") )
+    planetDetail = relationship(Planet,  back_populates="planet")
+    favoriteStatus = relationship(Favorite, back_populates="favorite")
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    uid = Column(Integer, primary_key=True)
+    vehicleName = Column(String(250))
+    vehicleNature= Column(String(250), ForeignKey("nature.natureName"))
+    vehicle = relationship(Nature,  back_populates="nature")
+
+class VehicleDetail(Base):
+    __tablename__ = 'vehicle_detail'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    uid = Column(Integer, primary_key=True)
+    vehicleName = Column(String(250))
+    vehicleNature = Column(String(250), ForeignKey("vehicle.vehicleNature") )
+    manufacturer = Column(String(250))
+    favorited = Column(Boolean, ForeignKey("favorite.favoriteStatus") )
+    vehicleDetail = relationship(Vehicle,  back_populates="vehicle")
+    favoriteStatus = relationship(Favorite, back_populates="favorite")
 
 
     def to_dict(self):
